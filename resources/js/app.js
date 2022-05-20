ccec8d401822e82cb33957648b3fac809fd52b0f
Your Name <your_email@whatever.com>
2022-05-20 16:32:03 +0300

    first try

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,41 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import axios from 'axios'
+import Vuetify from './plugins/vuetify'
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+Vue.use(VueRouter);
+Vue.component('App', require('./App.vue').default);
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+import store from "./store"
+import Main from './views-vue/Main.vue'
+import Login from './views-vue/Login.vue'
+import Register from './views-vue/Register.vue'
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+const router = new VueRouter({
+    mode: 'history',
+    routes: [{
+        path: '/login',
+        name: 'Login',
+        component: Login,
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register,
+    },
+    {
+        path: '/',
+        name: 'App',
+        component: App,
+    },
+    ]
+});
 
 const app = new Vue({
+    vuetify: Vuetify,
     el: '#app',
+    router,
+    store,
 });
---
+++ resources/js/views-vue/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>fddsggfd</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+
+export default{
+    
+}
+</script>
