4cf6da7d139ee3811d458d1c92011445e9138b1b
Kirill <kirilkenig@gmail.com>
2022-05-21 04:05:16 -0700

    fail

--- public/js/app.js
+++ public/js/app.js
@@ -5623,6 +5623,17 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
 
 /***/ }),
@@ -29979,7 +29990,30 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div")
+  return _c(
+    "v-app",
+    [
+      _c(
+        "v-card",
+        { attrs: { elevation: "0" } },
+        [
+          _c(
+            "v-row",
+            [
+              _c("v-icon", [_c("span", [_vm._v("ИИ")])]),
+              _vm._v(" "),
+              _c("v-card-title", [
+                _vm._v("\n                Иванов Иван Иванович\n            "),
+              ]),
+            ],
+            1
+          ),
+        ],
+        1
+      ),
+    ],
+    1
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
--- resources/js/views-vue/Profile.vue
+++ resources/js/views-vue/Profile.vue
@@ -1,10 +1,21 @@
 <template>
-    
+    <v-app>
+        <v-card elevation="0">
+            <v-row>
+                <v-icon>
+                    <span>ИИ</span>
+                </v-icon>
+                <v-card-title>
+                    Иванов Иван Иванович
+                </v-card-title>
+            </v-row>
+        </v-card>
+    </v-app>  
 </template>
 <script>
 
 export default {
-    
+
 }
 
 </script>
