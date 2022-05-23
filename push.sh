37246f55ff87f15c0ab42513102f7ee44fc3e26c
Kirill <kirilkenig@gmail.com>
2022-05-23 11:29:21 -0700

    adding

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
 if [ -n "$1" ];
 then
-    echo -e "\033[34mIndexing files"
+    echo -e "\033[34mIndexing files\e[97m"
     eval git add .
-    echo -e "\033[34mCommiting files"
+    echo -e "\033[34mCommiting files\e[97m"
     eval git commit -m "$1"
-    echo -e "\033[34mPushing files"
+    echo -e "\033[34mPushing files\e[97m"
     eval git push
 fi
 echo 0
