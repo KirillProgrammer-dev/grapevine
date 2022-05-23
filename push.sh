53c52467051a3b23c749b1cd34964608be556a15
Kirill <kirilkenig@gmail.com>
2022-05-23 11:37:03 -0700

    adding

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ then
     echo -e "\033[34mIndexing files\e[97m"
     eval git add .
     echo -e "\033[34mCommiting files\e[97m"
-    eval git commit -m "$1"
+    eval git commit -m "$*"
     echo -e "\033[34mPushing files\e[97m"
     eval git push
 fi
